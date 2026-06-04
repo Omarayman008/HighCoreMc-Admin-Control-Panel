17232c5446dfac9b85cf8f672659fc59fdc47404
Omar Ayman <omaralqtany81@gmail.com>
2026-06-04 20:07:57 +0300

    Update tickets.js - New ticket added

---
+++ tickets.js
@@ -0,0 +1,18 @@
+// بيانات التكتات
+window.ticketsData = [
+  {
+    "timestamp": "2026-06-04T17:07:56.844Z",
+    "ticketOwner": "<@1350531070222794804>",
+    "ticketOwnerId": "1350531070222794804",
+    "ticketName": "case-31",
+    "panelName": "Case",
+    "transcriptFile": "transcripts/case-31.html",
+    "claimedBy": "1350531070222794804",
+    "users": [
+      "<@1350531070222794804>"
+    ],
+    "responseTime": "N/A",
+    "ticketOwnerName": "agent_omar.dev",
+    "ticketOwnerDisplay": "ঔ Omar Ayman ঔ"
+  }
+];
