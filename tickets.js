6e5eb31f2f2180d55f2f352901de4fbde2614444
Omar Ayman <omaralqtany81@gmail.com>
2026-06-05 03:28:05 +0300

    Ticket closed: case-44

--- tickets.js
+++ tickets.js
@@ -1,20 +1,20 @@
 // Tickets Data
 window.ticketsData = [
   {
-    "timestamp": "2026-06-05T00:22:52.944Z",
-    "openedAt": "2026-06-05T00:13:00.000Z",
+    "timestamp": "2026-06-05T00:28:02.560Z",
+    "openedAt": "2026-06-05T00:27:00.000Z",
     "ticketOwner": "<@1350531070222794804>",
     "ticketOwnerId": "1350531070222794804",
-    "ticketName": "case-43",
+    "ticketName": "case-44",
     "panelName": "COMPLAINT",
-    "transcriptFile": "transcripts/case-43.html",
-    "transcriptUrl": "https://highcoremc-production.up.railway.app/view/transcript/43",
+    "transcriptFile": "transcripts/case-44.html",
+    "transcriptUrl": "https://highcoremc-production.up.railway.app/view/transcript/44",
     "claimedBy": "1350531070222794804",
-    "handlerUsername": "case-43",
+    "handlerUsername": "case-44",
     "users": [
       "<@1350531070222794804>"
     ],
-    "responseTime": "9m",
+    "responseTime": "1m",
     "ticketOwnerName": "agent_omar.dev",
     "ticketOwnerDisplay": "ঔ Omar Ayman ঔ"
   }
